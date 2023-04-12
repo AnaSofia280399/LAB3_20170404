7727bc8291255d81c7de3c6ad2019a0ba2a70718
Ana Sofia <alejandro.as@pucp.edu.pe>
2023-04-11 20:40:19 -0500

    primer commit

---
+++ src/main/java/com/example/lab3_20170404/Lab320170404Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20170404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320170404Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320170404Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_20170404/Lab320170404ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20170404;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320170404ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
